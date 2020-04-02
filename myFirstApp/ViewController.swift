7c58a3011f2147df73fbb6442555251db2976884
Still Beat <stillbeat73@gmail.com>
2020-04-02 20:16:59 +0400

    no message

--- myFirstApp/ViewController.swift
+++ myFirstApp/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
 
 }
-
