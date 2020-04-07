a5fe638488e7d197478822ded45871b6b089870b
Still Beat <stillbeat73@gmail.com>
2020-04-07 22:49:42 +0400

    Resize text

--- myFirstApp/ViewController.swift
+++ myFirstApp/ViewController.swift
@@ -14,23 +14,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var ButtonReset: UIButton!
     @IBOutlet weak var ButtonAddition: UIButton!
     var Count = 0
-    var Plus = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         MyLabel.text = String(Count)
+        MyLabel.textColor = .black
+        MyLabel.font = .systemFont(ofSize: 30)
         MyButton.setTitle("+", for: .normal)
         MyButton.setTitleColor(.black, for: .normal)
+        MyButton.titleLabel?.font = .systemFont(ofSize: 30)
         MyButton.layer.cornerRadius = 20
-        MyButton.backgroundColor = UIColor(red: 88/255, green: 195/255, blue: 37/255, alpha: 1)
+        MyButton.backgroundColor = UIColor(red: 88/255, green: 195/255, blue: 237/255, alpha: 1)
         ButtonReset.setTitle("Reset", for: .normal)
         ButtonReset.setTitleColor(.black, for: .normal)
         ButtonReset.layer.cornerRadius = 20
-        ButtonReset.backgroundColor = UIColor(red: 88/255, green: 195/255, blue: 37/255, alpha: 1)
+        ButtonReset.backgroundColor = UIColor(red: 88/255, green: 195/255, blue: 237/255, alpha: 1)
+        ButtonReset.titleLabel?.font = .systemFont(ofSize: 30)
         ButtonAddition.setTitle("-", for: .normal)
         ButtonAddition.setTitleColor(.black, for: .normal)
         ButtonAddition.layer.cornerRadius = 20
-        ButtonAddition.backgroundColor = UIColor(red: 88/255, green: 195/255, blue: 37/255, alpha: 1)
+        ButtonAddition.backgroundColor = UIColor(red: 88/255, green: 195/255, blue: 237/255, alpha: 1)
+        ButtonAddition.titleLabel?.font = .systemFont(ofSize: 30)
         if Count > 0 { Count = Count - 1}
         
         // Do any additional setup after loading the view.
@@ -39,7 +43,6 @@ class ViewController: UIViewController {
     @IBAction func MyButtonAction(_ sender: Any) {
        Count += 1
        MyLabel.text = String(Count)
-       MyButton.backgroundColor = (UIColor .red)
     }
     @IBAction func ButtonResetAction(_ sender: Any) {
         Count = 0
