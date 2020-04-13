1a4db4ea98d12abca77d090fa86ce28c03f065de
Still Beat <stillbeat73@gmail.com>
2020-04-13 21:27:25 +0400

    no message

---
+++ myFirstApp/NextPage/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+ //
+//  CustomTableViewCell.swift
+//  myFirstApp
+//
+//  Created by Sind Bad on 13.04.2020.
+//  Copyright © 2020 Still Beat. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
