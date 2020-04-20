f4b3386365b1c305a3db1001cacea6d8f84dc5b6
Still Beat <stillbeat73@gmail.com>
2020-04-20 19:43:04 +0400

    fix small errors

--- myFirstApp/NextPage/NextViewController.swift
+++ myFirstApp/NextPage/NextViewController.swift
@@ -108,7 +108,7 @@ UIViewController, UITableViewDataSource, UITableViewDelegate {
             let cell = tableView.dequeueReusableCell(withIdentifier: "covidTotal")! as! CovidCellTotal
             
             cell.labelOne.text = String(covids[indexPath.row].cases.recovered)
-            cell.labelTwo.text = String(covids[indexPath.row].test.total)
+            cell.labelTwo.text = String(covids[indexPath.row].tests.total)
             return cell
         }
         
